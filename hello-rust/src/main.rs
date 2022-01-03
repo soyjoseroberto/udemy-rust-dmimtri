7bf568d02f41bd11d8a169573d2e50629e8803e7
soyjoseroberto <jrodriguez18@gmail.com>
2022-01-02 23:10:54 -0500

    Some arithmetic operators.

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -3,6 +3,24 @@
 
 use std::mem;
 
+fn operators()
+{
+    println!("Operators section");
+    // Arithmetic
+    let mut a = 2 + 3 * 4; // +- */ as other languages, precedence applies
+    a = a + 1; // No ++ or -- operators, have to do it like this
+    a -= 2; // Rust does have += -= *= %= operators
+
+    println!("a = {}", a);
+
+    // No exp operator like ^ in other languages, use built-in fn instead
+    let b = 2.5; // f64 by type inference 
+    let b_cubed = f64::powi(b, 3);
+    let b_to_pi = f64::powf(b, std::f64::consts::PI);
+
+    println!("b_cubed = {}, b_to_pi = {}", b_cubed, b_to_pi);
+}
+
 fn main() {
     println!("Hello, world!");
 
@@ -17,4 +35,6 @@ fn main() {
     // Type inference
     let c = 123456789; // Output: 4 bytes = 32 bits = i32 type
     println!("c = {}, takes up {} bytes", c, mem::size_of_val(&c));
+
+    operators();
 }
