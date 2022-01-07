91b1d30548ddf32b7a3ae13a5e96fe71bf72b416
soyjoseroberto <jrodriguez18@gmail.com>
2022-01-06 23:38:38 -0500

    Added vectors function.

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -48,6 +48,51 @@ fn structures() {
     println!("Line from {} to {}", my_line.start.x, my_line.end.y);
 }
 
+fn vectors() {
+    /*
+    let mut a = Vec::new();
+
+    a.push(1);
+    a.push(2);
+    a.push(3);
+    */
+
+    let mut a = vec![1, 2, 3]; // [1;10]
+    println!("a = {:?}", a);
+
+    let idx/*:i32*/ = 0; // will not work with :i32
+                         // you need usize
+    println!("a[0] = {}", a[idx]);
+
+    // unsafe access
+    //println!("a[5] = {}", a[5]);
+
+    match a.get(5) {
+        Some(x) => println!("a[5] = {}", x),
+        None => println!("error, no such element"),
+    }
+
+    // iterate
+    for x in &a {
+        println!("{}", x);
+    }
+
+    // adding/removing
+    a.push(44);
+    println!("{:?}", a);
+
+    let last_elem = a.pop(); // can easily yield nothing
+    println!("last elem is {:?}, a = {:?}", last_elem, a);
+
+    // explain why this doesn't work
+    //let Some(last_value) = a.pop();
+
+    // print the elements in reverse order
+    while let Some(x) = a.pop() {
+        println!("{}", x);
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 
@@ -69,4 +114,7 @@ fn main() {
 
     // Function from an external file
     pattern_matching::demo();
+
+    // Vectors
+    vectors();
 }
