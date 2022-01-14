34127b60d7fb78ab5dbb55d4d723fb1979148bc2
soyjoseroberto <jrodriguez18@gmail.com>
2022-01-13 23:41:59 -0500

    Dynamic dispatch section added.

--- dispatch-examples/src/main.rs
+++ dispatch-examples/src/main.rs
@@ -18,9 +18,43 @@ impl Printable for String {
     }
 }
 
+// Dynamic dispatch
+struct Circle { radius: f64 }
+struct Square { side: f64 }
+
+trait Shape {
+  fn area(&self) -> f64;
+}
+
+impl Shape for Square {
+  fn area(&self) -> f64
+  {
+     self.side * self.side
+  }
+}
+
+impl Shape for Circle {
+  fn area(&self) -> f64
+  {
+    self.radius * self.radius * std::f64::consts::PI
+  }
+}
+
 fn main() {
     let a = 123;
     let b = "hello".to_string();
     println!("{}", a.format());
     println!("{}", b.format());
+
+    // Dynamic dispatch
+    let shapes:[& dyn Shape; 4] = [
+    &Circle{radius: 1.0},
+    &Square{side: 3.0},
+    &Circle{radius: 2.0},
+    &Square{side: 4.0}
+  ];
+  for (i, shape) in shapes.iter().enumerate()
+  {
+    println!("Shape #{} has area {}", i, shape.area());
+  }
 }
